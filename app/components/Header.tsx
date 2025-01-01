8a6d087667f164e0bfdc2d3e1de118e5234b5548
eiei114 <ekawano114@gmail.com>
2025-01-01 20:47:03 +0900

    update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,39 +8,44 @@ import { StyledLink } from "baseui/link";
 import { Button } from "baseui/button";
 import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
-
+import { useNavigate } from "@remix-run/react";
 export default () => {
     const { darkMode, toggleDarkMode } = useContext(ThemeContext);
+    const navigate = useNavigate();
 
   return (
     <HeaderNavigation>
-      <StyledNavigationList $align={ALIGN.center}>
+      <StyledNavigationList $align={ALIGN.left}>
         <StyledNavigationItem>
             <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
             Template App
             </h1>
         </StyledNavigationItem>
       </StyledNavigationList>
-      <StyledNavigationList $align={ALIGN.center}>
+      <StyledNavigationList $align={ALIGN.center}/>
+      <StyledNavigationList $align={ALIGN.right}>
         <StyledNavigationItem>
-          <StyledLink href="/">
+          <Button onClick={() => window.location.href = "/" }>
           <div className="h-[35px] w-[35px]">
-            <img src="/icons8-home-light.svg" alt="Home" className="dark:hidden"/>
-            <img src="/icons8-home-dark.svg" alt="Home" className="hidden dark:block"/>
+            <img src="/icons8-home-light.svg" alt="Home" className="hidden dark:block"/>
+            <img src="/icons8-home-dark.svg" alt="Home" className="dark:hidden"/>
           </div>
-          </StyledLink>
+          </Button>
         </StyledNavigationItem>
         <StyledNavigationItem>
-          <StyledLink href="https://github.com/eiei114/im-ts-frontend-template">
-          <div className="h-[50px] w-[50px]">
-            <img src="/icons8-github-light.svg" alt="GitHub" className="dark:hidden"/>
-            <img src="/icons8-github-dark.svg" alt="GitHub" className="hidden dark:block"/>
+          <Button onClick={() => window.open("https://github.com/eiei114/im-ts-frontend-template", "_blank") }>
+          <div className="h-[35px] w-[35px]">
+            <img src="/icons8-github-light.svg" alt="GitHub" className="hidden dark:block"/>
+            <img src="/icons8-github-dark.svg" alt="GitHub" className="dark:hidden"/>
           </div>
-          </StyledLink>
+          </Button>
         </StyledNavigationItem>
         <StyledNavigationItem>
         <Button onClick={toggleDarkMode}>
-        {darkMode ? "Light" : "Dark"}
+        <div className="h-[35px] w-[35px]">
+           <img src="/on-darkmode.svg" alt="Sun" className="hidden dark:block"/>
+           <img src="/on-lightmode.svg" alt="Moon" className="dark:hidden"/>
+        </div>
         </Button>
         </StyledNavigationItem>
       </StyledNavigationList>
