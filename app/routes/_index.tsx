c3f77c1178ff8d7de3916578ddc98da3428803c4
eiei114 <ekawano114@gmail.com>
2025-01-05 22:22:20 +0900

    update

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,8 @@
 import type { MetaFunction } from '@remix-run/node';
 import NameInput from '~/components/NameInput';
 import NumberInput from '~/components/NumberInput';
+import { useContext } from 'react';
+import { UserSecretContext } from '~/context/UserSecretContext';
 
 // ここでは、ページのメタデータを定義している。
 // ページのメタデータは、ページのタイトルやディスクリプションなどの情報を定義する。
@@ -12,15 +14,19 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const { token } = useContext(UserSecretContext);
+
   return (
     <div className="flex h-screen justify-center p-20">
       <div className="flex flex-col items-center gap-16 ">
         <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
           Welcome to Template App
         </h1>
-        <div className="flex space-x-4">
-          <NameInput />
-        </div>
+        {!token && (
+          <div className="flex space-x-4">
+            <NameInput />
+          </div>
+        )}
         <div className="flex space-x-4">
           <NumberInput />
         </div>
